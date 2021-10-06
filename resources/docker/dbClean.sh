f73890d4c09e08a9deb0e91e374d65542b6aba82
Tom Benjamins <tombenjamins@lostlemon.nl>
2021-10-06 11:31:45 +0200

    added rolling file logging by log4j to dbClean script

--- resources/docker/dbClean.sh
+++ resources/docker/dbClean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/ebms
-java -cp ebms-admin.jar nl.clockwork.ebms.admin.DBClean -configDir /conf/ -cmd messages -retentionDays 90 >>/logs/dbClean.log
+java -Dlog4j.configurationFile=log4j2.dbClean.xml -cp ebms-admin.jar nl.clockwork.ebms.admin.DBClean -configDir /conf/ -cmd messages -retentionDays 90 
